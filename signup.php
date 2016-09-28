77643afc11752b8e0cc145a42dd25df9192df477
Simone Lee <simonelee@live.dk>
2016-09-28 10:02:03 +0200

    Connected with the database
    Removed the testing echo of the form input and make the connection to
    the database of the input.

--- signup.php
+++ signup.php
@@ -6,8 +6,7 @@ $last = $_POST['last'];
 $uid = $_POST['uid'];
 $pwd = $_POST['pwd'];
 
-echo $first."<br>";
-echo $last."<br>";
-echo $uid."<br>";
-echo $pwd."<br>";
+$sql = "INSERT INTO user (first, last, uid, pwd) VALUES ('$first', '$last', '$fuid', '$pwd')";
+$result = mysqli_query($conn, $sql);
+
 ?>
