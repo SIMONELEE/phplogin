606d58d09502042edde12883db9dc8c4f830056d
Simone Lee <simonelee@live.dk>
2016-09-28 09:23:42 +0200

    Sign Up Form
    Made the form for the sign up.

--- index.php
+++ index.php
@@ -4,8 +4,16 @@
 <meta charset="UTF-8">
 <title>PHP Login System</title>
 </head>
-
 <body>
 <h1>PHP Login System</h1>
+
+<form>
+	<input type="text" name="first" placeholder="First Name">
+    <input type="text" name="last" placeholder="Last Name">
+    <input type="text" name="uid" placeholder="Username">
+    <input type="password" name="pwd" placeholder="Password">
+    <button type="submit">SIGN UP</button>
+</form>
+
 </body>
 </html>
