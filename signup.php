481b0e2defc9c1aea1647ee1594ddc2d17b28fb0
Simone Lee <simonelee@live.dk>
2016-09-28 09:49:14 +0200

    Signup Form
    Connected to the dbh.php file and test the signup form with echo.

--- signup.php
+++ signup.php
@@ -1,3 +1,13 @@
 <?php 
+include 'dbh.php';
 
+$first = $_POST['first'];
+$last = $_POST['last'];
+$uid = $_POST['uid'];
+$pwd = $_POST['pwd'];
+
+echo $first."<br>";
+echo $last."<br>";
+echo $uid."<br>";
+echo $pwd."<br>";
 ?>
